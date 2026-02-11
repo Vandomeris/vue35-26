1435746b01cd78e1a5dc67c9da21c80a583ed993
Vando <vandomeri@gmail.com>
2026-02-11 12:17:29 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,10 @@ const showAdd = ref(false)
 
 const newTodo = ref('')
 
+const showModal = ref(false)
+
+const editingInput = ref('')
+
 function addTodo() {
   todos.value.push({
     id: Date.now(),
@@ -19,6 +23,24 @@ function addTodo() {
   })
 
   newTodo.value = ''
+}
+
+function deleteTodo(id: number) {
+  const index = todos.value.findIndex((todo) => todo.id === id)
+
+  if (index !== -1) {
+    todos.value.splice(index, 1)
+  }
+}
+
+function editTodo(id: number) {
+  const index = todos.value.findIndex((todo) => todo.id === id)
+
+  if (index !== -1) {
+    editingInput.value = todos.value[index]!.title
+  }
+
+  showModal.value = true
 }
 </script>
 
@@ -46,6 +68,17 @@ function addTodo() {
     <span>{{ todo.id }}</span>
     <span>{{ todo.title }}</span>
     <input type="checkbox" v-model="todo.completed" />
+    <button @click="deleteTodo(todo.id)">Delete</button>
+    <button @click="editTodo(todo.id)">Edit</button>
+  </div>
+  <div v-if="showModal" class="modal-overlay">
+    <div class="modal">
+      <h3>Редактировать дело</h3>
+      <form>
+        <input v-model="editingInput" type="text" />
+        <button>Сохранить</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -60,7 +93,26 @@ h1 {
   column-gap: 40px;
 }
 
-.completed {
+.completed span {
   text-decoration: line-through;
+}
+
+.modal-overlay {
+  background-color: #0000005a;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  background-color: white;
+  max-width: 500px;
+  width: 100%;
+  padding: 25px;
 }
 </style>
